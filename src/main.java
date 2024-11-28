e6e500c040307d89c016f6526f4a4f76f2ce248e
MariaT <maria.l.thao@gmail.com>
2024-11-27 23:44:01 -0600

    Beginning code

--- src/main.java
+++ src/main.java
@@ -1,7 +1,23 @@
 import java.util.*;
 
 public class main {
-    public static void main{
+    public static void main(String[] args) {
+        Scanner playChoice;
+
+        playChoice = new Scanner(System.in);
+        System.out.println("Would you like to play Rock Paper Scissors? Y or N");
+
+        String userChoiceGame = playChoice.nextLine();
+
+        if (userChoiceGame.equals("y") || userChoiceGame.equals("Y")){
+            System.out.println("Starting Game");//run game in loop
+        }
+        else if (userChoiceGame.equals("n") || userChoiceGame.equals("N")) {
+            System.out.println("Exiting...");
+        } else {
+            System.out.println("Please enter vaild answer.");
+        }
+
 
     }
 }
